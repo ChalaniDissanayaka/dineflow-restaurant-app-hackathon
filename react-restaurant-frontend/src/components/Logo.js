f817bb9ceaf9fdb7db6e14ead29b049e4a6e0e24
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-10-09 17:20:02 +1100

    Added navbar to Home page

---
+++ react-restaurant-frontend/src/components/Logo.js
@@ -0,0 +1,12 @@
+import './Logo.css';
+
+function Logo() {
+    return (
+        <div className="logo">
+            <img src="./DineFlow.png" alt="" />
+            <h1>Restaurant</h1>
+        </div>
+    );
+}
+
+export default Logo;
--- react-restaurant-frontend/src/pages/Home.js
+++ react-restaurant-frontend/src/pages/Home.js
@@ -1,8 +1,54 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Button, IconButton } from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
+import Logo from '../components/Logo';
 import './Home.css';
 
 function Home() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <h2>Home</h2>
+        <div className="home-container">
+            <div className="home-nav">
+                <Logo />
+                <div className="menu-desktop">
+                    <Link to="/login">
+                        <Button colorScheme='teal' variant='solid' size='md'>Login</Button>
+                    </Link>
+                    <Link to="/register">
+                        <Button colorScheme='yellow' variant='solid'>Register</Button>
+                    </Link>
+                </div>
+
+                {/* Mobile Menu Icon */}
+                <div className="menu-mobile-icon">
+                    <IconButton 
+                        icon={<HamburgerIcon w={8} h={8} />} 
+                        colorScheme='white'
+                        variant="solid" 
+                        onClick={toggleMenu} 
+                        aria-label="Open Menu" 
+                    />
+                </div>
+            </div>
+
+            {/* Mobile Dropdown Menu */}
+            {menuOpen && (
+                <div className="menu-mobile">
+                    <Link to="/login">
+                        <Button colorScheme='teal' variant='solid' size='md'>Login</Button>
+                    </Link>
+                    <Link to="/register">
+                        <Button colorScheme='yellow' variant='solid'>Register</Button>
+                    </Link>
+                </div>
+            )}
+        </div>    
     );
 }
 
