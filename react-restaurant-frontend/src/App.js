a3cfaf74979dfec431f1771436cec905b088e770
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-10-10 23:41:21 +1100

    Added route to Register and Login pages

--- react-restaurant-frontend/src/App.js
+++ react-restaurant-frontend/src/App.js
@@ -3,6 +3,8 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import { Toaster } from 'react-hot-toast';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
       </BrowserRouter>
       <Toaster position='bottom-right' />
