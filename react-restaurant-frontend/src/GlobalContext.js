5de9762cee2c3edb8d4e50c35364e0a93c55eec0
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-10-15 17:40:48 +1100

    Implement user authentication flow with fetchUser in useEffect and RequireAuth component to protect routes

--- react-restaurant-frontend/src/GlobalContext.js
+++ react-restaurant-frontend/src/GlobalContext.js
@@ -1,20 +1,37 @@
-import { createContext, useContext, useState } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+import Loading from "./components/common/Loading";
+import { api } from "./config";
 
 const GlobalContext = createContext();
 
 function GlobalProvider({ children }) {
-    const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const result = await axios.get(`${api}/auth/me`, {
+          withCredentials: true,
+        });
+        setUser(result.data);
+      } catch (err) {}
+      setLoading(false);
+    };
 
-    return (
-        <GlobalContext.Provider value={{ user, setUser }}> 
-            {children} 
-        </GlobalContext.Provider>
-    );
+    fetchUser();
+  }, []);
+
+  return (
+    <GlobalContext.Provider value={{ user, setUser }}>
+      {loading ? <Loading height="100vh" /> : <>{children}</>}
+    </GlobalContext.Provider>
+  );
 }
 
 export default GlobalProvider;
 
 export function useGlobal() {
-    return useContext(GlobalContext);
+  return useContext(GlobalContext);
 }
--- react-restaurant-frontend/src/components/layouts/AdminLayout.js
+++ react-restaurant-frontend/src/components/layouts/AdminLayout.js
@@ -1,17 +1,19 @@
+import RequireAuth from '../common/RequireAuth';
 import './AdminLayout.css';
 import AdminNavbar from './AdminNavbar';
 import Sidebar from './Sidebar';
 
 function AdminLayout({ children }) {
     return (
-        <div className="layout-container">
-            <AdminNavbar />
-            <div className="inner-layout">
-                <Sidebar />
-                {children}
+        <RequireAuth>
+            <div className="layout-container">
+                <AdminNavbar />
+                <div className="inner-layout">
+                    <Sidebar />
+                    {children}
+                </div>
             </div>
-            
-        </div>
+        </RequireAuth>
     );
 }
 
