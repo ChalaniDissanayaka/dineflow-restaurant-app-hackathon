d087e1617934191ae81df798bbc8ab8c98ea6a1a
Chalani Dissanayaka <15415@coderacademy.edu.au>
2024-10-15 18:10:17 +1100

    Refactor MainMenu component to use useRef for component mount check and optimize fetchItems logic

--- react-restaurant-frontend/src/components/MainMenu.js
+++ react-restaurant-frontend/src/components/MainMenu.js
@@ -1,63 +1,64 @@
-import { useState, useEffect } from 'react';
-import './MainMenu.css';
-import axios from 'axios';
-import {api} from '../config';
-import MenuItem from './MenuItem';
-import toast from 'react-hot-toast';
+import { useEffect, useRef, useState } from "react";
+import "./MainMenu.css";
+import axios from "axios";
+import { api } from "../config";
+import MenuItem from "./MenuItem";
+import toast from "react-hot-toast";
 
+function MainMenu({ cartItems, setCartItems }) {
+  const [items, setItems] = useState([]);
+  const mountRef = useRef(true);
 
-function MainMenu({cartItems, setCartItems}) {
-    const [items, setItems] = useState([]);
+  useEffect(() => {
+    if (mountRef.current) {
+      mountRef.current = false;
+      const fetchItems = async () => {
+        const result = await axios.get(`${api}/item/all`);
+        setItems(result.data);
+      };
 
-    useEffect(() => {
-        const fetchItems = async () => {
-            // fetch all Menu Items for the API
-            // const result = await axios.get("http://localhost:4200/item/all");
-            const result = await axios.get(`${api}/item/all`);
-            // console.log(result);
-            setItems(result.data);
-        };
+      fetchItems();
+    }
+  }, []);
 
-        fetchItems();
-    }, []);
+  const handleClick = (clickedItem) => {
+    const itemFound = cartItems.find((item) => {
+      if (item.id === clickedItem.id) {
+        return true;
+      } else {
+        return false;
+      }
+    });
 
-    const handleClick = (clickedItem) => {
-        // console.log(clickedItem);
+    if (itemFound) {
+      toast.error("Item already added");
+      return;
+    }
 
-        const itemFound = cartItems.find((item) => {
-            if (item.id === clickedItem.id) {
-                return true;
-            } else {
-                return false;
-            }
-        })
-
-        if (itemFound) {
-            toast.error("Item already added");
-            return;
-        }
-
-
-        const newCartItems = [...cartItems];
-        const newItem = {
-            ...clickedItem,
-            qty: 1
-        }
-        newCartItems.push(newItem);
-        setCartItems(newCartItems);
+    const newCartItems = [...cartItems];
+    const newItem = {
+      ...clickedItem,
+      qty: 1,
     };
+    newCartItems.push(newItem);
+    setCartItems(newCartItems);
+  };
 
-    return (
-        <div className="main-menu-container">
-            {items.map((item) => {
-                return (
-                    <MenuItem key={item.id} item={item} onClick={() => {
-                        handleClick(item);
-                    }} />
-                )
-            })}
-        </div>
-    );
+  return (
+    <div className="main-menu-container">
+      {items.map((item) => {
+        return (
+          <MenuItem
+            key={item.id}
+            item={item}
+            onClick={() => {
+              handleClick(item);
+            }}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default MainMenu;
